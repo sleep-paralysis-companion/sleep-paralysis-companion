46ef417db5eca87db62759993e7c31c6526b6404
Satyam Shree <satyamshree227@gmail.com>
2026-08-01 16:43:31 +0530

    Isolate UI tests from unavailable OS stores

--- ios/Sources/App/AppCompositionRoot.swift
+++ ios/Sources/App/AppCompositionRoot.swift
@@ -18,9 +18,9 @@ enum AppCompositionRoot {
             "SPC_DISABLE_AUTH_CONFIGURATION"
         ] == "1"
         #if DEBUG
-            if let value = ProcessInfo.processInfo.environment["SPC_UI_TEST_AUTHENTICATED_USER_ID"],
-               let userID = UUID(uuidString: value)
-            {
+            let uiTestUserID = ProcessInfo.processInfo.environment["SPC_UI_TEST_AUTHENTICATED_USER_ID"]
+                .flatMap(UUID.init(uuidString:))
+            if let userID = uiTestUserID {
                 authentication = UITestOAuthSessionService(userID: userID)
                 remote = nil
             } else {
@@ -62,15 +62,62 @@ enum AppCompositionRoot {
             }
         #endif
 
+        let store: IntegratedPhase1Store
+        #if DEBUG
+            if uiTestUserID != nil {
+                store = IntegratedPhase1Store(
+                    location: LocalStoreLocation(namespace: namespace),
+                    protection: UITestProtectedFileApplicator(),
+                    preferences: AccountBoundPreferencesStore(keychain: UITestKeychainClient()),
+                    remote: nil
+                )
+            } else {
+                store = IntegratedPhase1Store(
+                    location: LocalStoreLocation(namespace: namespace),
+                    remote: remote
+                )
+            }
+        #else
+            store = IntegratedPhase1Store(
+                location: LocalStoreLocation(namespace: namespace),
+                remote: remote
+            )
+        #endif
+
         return AppModel(
             environment: environment,
             accessPolicy: AccessPolicy(),
-            store: IntegratedPhase1Store(
-                location: LocalStoreLocation(namespace: namespace),
-                remote: remote
-            ),
+            store: store,
             authentication: authentication,
             logger: logger
         )
     }
 }
+
+#if DEBUG
+    private nonisolated struct UITestProtectedFileApplicator: ProtectedFileApplying {
+        func applyProtection(to url: URL, kind: ProtectedFileKind) throws {
+            _ = url
+            _ = kind
+        }
+    }
+
+    private nonisolated struct UITestKeychainClient: KeychainClient {
+        func read(service: String, account: String) throws -> Data? {
+            _ = service
+            _ = account
+            return nil
+        }
+
+        func write(_ data: Data, service: String, account: String) throws {
+            _ = data
+            _ = service
+            _ = account
+        }
+
+        func delete(service: String, account: String) throws {
+            _ = service
+            _ = account
+        }
+    }
+#endif
